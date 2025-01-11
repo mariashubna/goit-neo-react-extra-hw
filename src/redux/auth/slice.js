fee23614b0cafe9e695efd3fd27fda35e5f84ac0
mariashubna <mariashubna@gmail.com>
2025-01-11 20:45:50 +0100

    /

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -6,10 +6,6 @@ import {
   registerOperation,
 } from "./operations";
 
-const handlePending = (state) => {
-  state.isLoggedIn = true;
-};
-
 const handleRejected = (state, action) => {
   state.isLoggedIn = false;
   state.error = action.payload;
@@ -29,21 +25,18 @@ const slice = createSlice({
   },
   extraReducers: (builder) => {
     builder
-      .addCase(registerOperation.pending, handlePending)
       .addCase(registerOperation.fulfilled, (state, { payload }) => {
         state.user = payload.user;
         state.token = payload.token;
         state.isLoggedIn = true;
       })
       .addCase(registerOperation.rejected, handleRejected)
-      .addCase(logInOperation.pending, handlePending)
       .addCase(logInOperation.fulfilled, (state, { payload }) => {
         state.user = payload.user;
         state.token = payload.token;
         state.isLoggedIn = true;
       })
       .addCase(logInOperation.rejected, handleRejected)
-      .addCase(logOutOperation.pending, handlePending)
       .addCase(logOutOperation.fulfilled, (state) => {
         state.user = {
           name: null,
